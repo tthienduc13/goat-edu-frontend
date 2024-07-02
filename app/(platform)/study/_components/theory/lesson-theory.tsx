35831d7fb58d989689007eb21b88e6cafea37de1
Kiamnq <lamnq.dev@gmail.com>
2024-07-03 00:19:59 +0700

    fix ui

--- app/(platform)/study/_components/theory/lesson-theory.tsx
+++ app/(platform)/study/_components/theory/lesson-theory.tsx
@@ -5,7 +5,6 @@ import sampleImage from "@/assets/sample2.png";
 import katex from "katex";
 import "katex/dist/katex.min.css";
 import Image from "next/image";
-import TheorySection from "./theory-section";
 import TheoryLoading from "./theory-loading";
 
 interface LatexRendererProps {
@@ -24,15 +23,45 @@ const LessonTheory = ({ lessonId, lessonName }: LessonTheoryProps) => {
     isLoading: theoryLoading,
     error: theoryError,
   } = useTheoryByLesson(lessonId, user?.token as string);
+
+  const LatexRenderer: React.FC<LatexRendererProps> = ({ latex }) => {
+    useEffect(() => {
+      const elements = document.querySelectorAll("[data-latex]");
+      elements.forEach((element) => {
+        const latex = element.getAttribute("data-latex");
+        if (latex) {
+          katex.render(latex, element as HTMLElement, {
+            throwOnError: false,
+          });
+        }
+      });
+    }, [latex]);
+
+    return <div dangerouslySetInnerHTML={{ __html: latex }} />;
+  };
+
   if (theoryLoading) {
     return <TheoryLoading />;
   }
 
   return (
-    <div className="space-y-4">
+    <div className="space-y-8">
       <h1 className="text-3xl font-semibold">{lessonName}</h1>
       {theoryData?.map((theory) => (
-        <TheorySection key={theory.id} theory={theory} />
+        <div key={theory.id}>
+          <h3 className="texl-xl  font-medium"> {theory.theoryName}</h3>
+          <LatexRenderer latex={theory.theoryContent} />
+          <div className="w-full flex justify-center">
+            {theory.image && (
+              <Image
+                src={sampleImage}
+                height={400}
+                width={400}
+                alt="theory image"
+              ></Image>
+            )}
+          </div>
+        </div>
       ))}
     </div>
   );
